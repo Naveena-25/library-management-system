9139da9929ced9d02427ce9a6b7b800deaa69785
Naveena-25 <naveena25.gurram@gmail.com>
2020-05-06 20:09:46 +0530

    commit changes

--- librarymanagementhibernate/librarymanagementhibernate/src/main/java/com/capgemini/librarymanagementhibernate/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.librarymanagementhibernate.controller;
-
-public class Controller {
-	
-	public static void main(String[] args) {
-		
-		LibraryUtility.libraryManagementUtility();
-	}
-}
--- librarymanagementhibernate/librarymanagementhibernate/src/main/java/com/capgemini/librarymanagementhibernate/exception/LMSException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.librarymanagementhibernate.exception;
-
-public class LMSException extends RuntimeException{
-	public LMSException(String msg) {
-		super(msg);
-	}
-}
